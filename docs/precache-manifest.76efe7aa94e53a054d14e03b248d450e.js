c7a2009fdf5c0e35d89c8d371e190c4dd7d9f1d9
cyaxares <me@siamand.cc>
2019-12-21 03:04:56 +0100

    feat: fix dummy vueJS pwas icons

---
+++ docs/precache-manifest.76efe7aa94e53a054d14e03b248d450e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1b622c8b7aca211626",
+    "url": "/OpenFork/css/app.766d66aa.css"
+  },
+  {
+    "revision": "6712d7ce72b6566750e792b4ca208987",
+    "url": "/OpenFork/index.html"
+  },
+  {
+    "revision": "ef1b622c8b7aca211626",
+    "url": "/OpenFork/js/app.60eeb12e.js"
+  },
+  {
+    "revision": "495495de823aaf578b98",
+    "url": "/OpenFork/js/chunk-vendors.0b7bf644.js"
+  },
+  {
+    "revision": "f790253a82385bdf0e9733fdfec36fb8",
+    "url": "/OpenFork/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/OpenFork/robots.txt"
+  }
+]);
